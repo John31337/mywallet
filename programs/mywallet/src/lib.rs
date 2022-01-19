290315de24735e824070437b0b99b0021ed46320
John31337 <95174336+John31337@users.noreply.github.com>
2022-01-19 11:09:20 +0800

    create

--- programs/mywallet/src/lib.rs
+++ programs/mywallet/src/lib.rs
@@ -1,5 +1,5 @@
 use anchor_lang::prelude::*;
-use anchor_spl::token::{self, CloseAccount, Mint, SetAuthority, TokenAccount, Transfer};
+use anchor_spl::token::{ self, Mint, SetAuthority, TokenAccount, Transfer};
 use spl_token::instruction::AuthorityType;
 
 declare_id!("AJKJVBUiLsE453wiKgowNtgLtrZUVv5GRsGuBrXY9GCq");
@@ -7,106 +7,51 @@ declare_id!("AJKJVBUiLsE453wiKgowNtgLtrZUVv5GRsGuBrXY9GCq");
 // Program Account (fully implemented)
 
 #[account]
-pub struct EscrowAccount {
+pub struct MarketAccount {
     pub initializer_key: Pubkey,
-    pub initializer_deposit_token_account: Pubkey,
-    pub initializer_receive_token_account: Pubkey,
-    pub initializer_amount: u64,
-    pub taker_amount: u64,
+    pub initializer_market_account: Pubkey,
+    pub amount: u64,
 }
 
 #[program]
-pub mod mywallet {
+pub mod marketplace {
     use super::*;
 
-    const ESCROW_PDA_SEED: &[u8] = b"escrow";
+    const MARKET_PDA_SEED: &[u8] = b"marketplace";
 
     pub fn initialize(
         ctx: Context<Initialize>,
         _vault_account_bump: u8,
         initializer_amount: u64,
-        taker_amount: u64,
     ) -> ProgramResult {
-        ctx.accounts.escrow_account.initializer_key = *ctx.accounts.initializer.key;
-        ctx.accounts
-            .escrow_account
-            .initializer_deposit_token_account = *ctx
-            .accounts
-            .initializer_deposit_token_account
-            .to_account_info()
-            .key;
-        ctx.accounts
-            .escrow_account
-            .initializer_receive_token_account = *ctx
-            .accounts
-            .initializer_receive_token_account
-            .to_account_info()
-            .key;
-        ctx.accounts.escrow_account.initializer_amount = initializer_amount;
-        ctx.accounts.escrow_account.taker_amount = taker_amount;
+        ctx.accounts.market_account.initializer_key = *ctx.accounts.initializer.key;
+        ctx.accounts.market_account.amount = initializer_amount;
 
-        let (vault_authority, _vault_authority_bump) =
-            Pubkey::find_program_address(&[ESCROW_PDA_SEED], ctx.program_id);
+        let (_vault_authority, _vault_authority_bump) =
+        Pubkey::find_program_address(&[MARKET_PDA_SEED], ctx.program_id);
+
         token::set_authority(
             ctx.accounts.into_set_authority_context(),
             AuthorityType::AccountOwner,
-            Some(vault_authority),
+            Some(_vault_authority),
         )?;
+
+        Ok(())
+    }
+
+    pub fn buy(ctx: Context<Buy>) -> ProgramResult {
+        let (_vault_authority, _vault_authority_bump) =
+            Pubkey::find_program_address(&[MARKET_PDA_SEED], ctx.program_id);
 
         token::transfer(
             ctx.accounts.into_transfer_to_pda_context(),
-            ctx.accounts.escrow_account.initializer_amount,
+            ctx.accounts.market_account.amount,
         )?;
 
         Ok(())
     }
 
-    pub fn cancel(ctx: Context<Cancel>) -> ProgramResult {
-        let (_vault_authority, vault_authority_bump) =
-            Pubkey::find_program_address(&[ESCROW_PDA_SEED], ctx.program_id);
-        let authority_seeds = &[&ESCROW_PDA_SEED[..], &[vault_authority_bump]];
-
-        token::transfer(
-            ctx.accounts
-                .into_transfer_to_initializer_context()
-                .with_signer(&[&authority_seeds[..]]),
-            ctx.accounts.escrow_account.initializer_amount,
-        )?;
-
-        token::close_account(
-            ctx.accounts
-                .into_close_context()
-                .with_signer(&[&authority_seeds[..]]),
-        )?;
-
-        Ok(())
-    }
-
-    pub fn exchange(ctx: Context<Exchange>) -> ProgramResult {
-        let (_vault_authority, vault_authority_bump) =
-            Pubkey::find_program_address(&[ESCROW_PDA_SEED], ctx.program_id);
-        let authority_seeds = &[&ESCROW_PDA_SEED[..], &[vault_authority_bump]];
-
-        token::transfer(
-            ctx.accounts.into_transfer_to_initializer_context(),
-            ctx.accounts.escrow_account.taker_amount,
-        )?;
-
-        token::transfer(
-            ctx.accounts
-                .into_transfer_to_taker_context()
-                .with_signer(&[&authority_seeds[..]]),
-            ctx.accounts.escrow_account.initializer_amount,
-        )?;
-
-        token::close_account(
-            ctx.accounts
-                .into_close_context()
-                .with_signer(&[&authority_seeds[..]]),
-        )?;
-
-        Ok(())
-    }
+    
 }
 // Instructions (fully implementated)
 
@@ -118,89 +63,41 @@ pub struct Initialize<'info> {
     pub mint: Account<'info, Mint>,
     #[account(
         init,
-        seeds = [b"token-seed".as_ref()],
+        seeds = [b"marketplace".as_ref()],
         bump = vault_account_bump,
         payer = initializer,
         token::mint = mint,
         token::authority = initializer,
     )]
     pub vault_account: Account<'info, TokenAccount>,
-    #[account(
-        mut,
-        constraint = initializer_deposit_token_account.amount >= initializer_amount
-    )]
-    pub initializer_deposit_token_account: Account<'info, TokenAccount>,
-    pub initializer_receive_token_account: Account<'info, TokenAccount>,
     #[account(zero)]
-    pub escrow_account: Box<Account<'info, EscrowAccount>>,
+    pub market_account: Box<Account<'info, MarketAccount>>,
     pub system_program: AccountInfo<'info>,
-    pub rent: Sysvar<'info, Rent>,
     pub token_program: AccountInfo<'info>,
+    pub rent: Sysvar<'info, Rent>,
 }
 
 #[derive(Accounts)]
-pub struct Cancel<'info> {
+pub struct Buy<'info> {
     #[account(mut, signer)]
     pub initializer: AccountInfo<'info>,
-    #[account(mut)]
-    pub vault_account: Account<'info, TokenAccount>,
-    pub vault_authority: AccountInfo<'info>,
-    #[account(mut)]
-    pub initializer_deposit_token_account: Account<'info, TokenAccount>,
+    pub mint: Account<'info, Mint>,
     #[account(
         mut,
-        constraint = escrow_account.initializer_key == *initializer.key,
-        constraint = escrow_account.initializer_deposit_token_account == *initializer_deposit_token_account.to_account_info().key,
+        constraint = market_account.initializer_key == *initializer.key,
         close = initializer
     )]
-    pub escrow_account: Box<Account<'info, EscrowAccount>>,
+    pub market_account: Box<Account<'info, MarketAccount>>,
+    #[account(mut)]
+    pub vault_account: Account<'info, TokenAccount>,
     pub token_program: AccountInfo<'info>,
 }
 
-#[derive(Accounts)]
-pub struct Exchange<'info> {
-    #[account(signer)]
-    pub taker: AccountInfo<'info>,
-    #[account(mut)]
-    pub taker_deposit_token_account: Account<'info, TokenAccount>,
-    #[account(mut)]
-    pub taker_receive_token_account: Account<'info, TokenAccount>,
-    #[account(mut)]
-    pub initializer_deposit_token_account: Account<'info, TokenAccount>,
-    #[account(mut)]
-    pub initializer_receive_token_account: Account<'info, TokenAccount>,
-    #[account(mut)]
-    pub initializer: AccountInfo<'info>,
-    #[account(
-        mut,
-        constraint = escrow_account.taker_amount <= taker_deposit_token_account.amount,
-        constraint = escrow_account.initializer_deposit_token_account == *initializer_deposit_token_account.to_account_info().key,
-        constraint = escrow_account.initializer_receive_token_account == *initializer_receive_token_account.to_account_info().key,
-        constraint = escrow_account.initializer_key == *initializer.key,
-        close = initializer
-    )]
-    pub escrow_account: Box<Account<'info, EscrowAccount>>,
-    #[account(mut)]
-    pub vault_account: Account<'info, TokenAccount>,
-    pub vault_authority: AccountInfo<'info>,
-    pub token_program: AccountInfo<'info>,
-}
+
 
 // Utils (fully implemented)
 
 impl<'info> Initialize<'info> {
-    fn into_transfer_to_pda_context(&self) -> CpiContext<'_, '_, '_, 'info, Transfer<'info>> {
-        let cpi_accounts = Transfer {
-            from: self
-                .initializer_deposit_token_account
-                .to_account_info()
-                .clone(),
-            to: self.vault_account.to_account_info().clone(),
-            authority: self.initializer.clone(),
-        };
-        CpiContext::new(self.token_program.clone(), cpi_accounts)
-    }
-
     fn into_set_authority_context(&self) -> CpiContext<'_, '_, '_, 'info, SetAuthority<'info>> {
         let cpi_accounts = SetAuthority {
             account_or_mint: self.vault_account.to_account_info().clone(),
@@ -210,62 +107,13 @@ impl<'info> Initialize<'info> {
     }
 }
 
-impl<'info> Cancel<'info> {
-    fn into_transfer_to_initializer_context(
-        &self,
-    ) -> CpiContext<'_, '_, '_, 'info, Transfer<'info>> {
+impl<'info> Buy<'info> {
+    fn into_transfer_to_pda_context(&self) -> CpiContext<'_, '_, '_, 'info, Transfer<'info>> {
         let cpi_accounts = Transfer {
-            from: self.vault_account.to_account_info().clone(),
-            to: self
-                .initializer_deposit_token_account
-                .to_account_info()
-                .clone(),
-            authority: self.vault_authority.clone(),
-        };
-        CpiContext::new(self.token_program.clone(), cpi_accounts)
-    }
-
-    fn into_close_context(&self) -> CpiContext<'_, '_, '_, 'info, CloseAccount<'info>> {
-        let cpi_accounts = CloseAccount {
-            account: self.vault_account.to_account_info().clone(),
-            destination: self.initializer.clone(),
-            authority: self.vault_authority.clone(),
+            from: self.initializer.to_account_info().clone(),
+            to: self.vault_account.to_account_info().clone(),
+            authority: self.initializer.clone(),
         };
         CpiContext::new(self.token_program.clone(), cpi_accounts)
     }
 }
-
-impl<'info> Exchange<'info> {
-    fn into_transfer_to_initializer_context(
-        &self,
-    ) -> CpiContext<'_, '_, '_, 'info, Transfer<'info>> {
-        let cpi_accounts = Transfer {
-            from: self.taker_deposit_token_account.to_account_info().clone(),
-            to: self
-                .initializer_receive_token_account
-                .to_account_info()
-                .clone(),
-            authority: self.taker.clone(),
-        };
-        CpiContext::new(self.token_program.clone(), cpi_accounts)
-    }
-
-    fn into_transfer_to_taker_context(&self) -> CpiContext<'_, '_, '_, 'info, Transfer<'info>> {
-        let cpi_accounts = Transfer {
-            from: self.vault_account.to_account_info().clone(),
-            to: self.taker_receive_token_account.to_account_info().clone(),
-            authority: self.vault_authority.clone(),
-        };
-        CpiContext::new(self.token_program.clone(), cpi_accounts)
-    }
-
-    fn into_close_context(&self) -> CpiContext<'_, '_, '_, 'info, CloseAccount<'info>> {
-        let cpi_accounts = CloseAccount {
-            account: self.vault_account.to_account_info().clone(),
-            destination: self.initializer.clone(),
-            authority: self.vault_authority.clone(),
-        };
-        CpiContext::new(self.token_program.clone(), cpi_accounts)
-    }
-}
-
--- tests/mytest.ts
+++ tests/mytest.ts
@@ -2,7 +2,7 @@
 
 import * as anchor from '@project-serum/anchor';
 import { Program } from '@project-serum/anchor';
-import { Mywallet } from '../target/types/mywallet';
+import { Marketplace } from '../target/types/marketplace';
 import { PublicKey, SystemProgram, Transaction } from '@solana/web3.js';
 import { TOKEN_PROGRAM_ID, Token } from "@solana/spl-token";
 import { assert } from "chai";
@@ -16,26 +16,25 @@ describe('anchor-escrow', () => {
   const provider = anchor.Provider.env();
   anchor.setProvider(provider);
 
-  const program = anchor.workspace.Mywallet as Program<Mywallet>;
+  const program = anchor.workspace.Marketplace as Program<Marketplace>;
 
-  let mintA = null;
-  let mintB = null;
-  let initializerTokenAccountA = null;
-  let initializerTokenAccountB = null;
-  let takerTokenAccountA = null;
-  let takerTokenAccountB = null;
+  let mint = null;
+  let initializerTokenAccount1 = null;
+  let initializerTokenAccount2 = null;
   let vault_account_pda = null;
   let vault_account_bump = null;
   let vault_authority_pda = null;
-
+  
+  
   const takerAmount = 1000;
   const initializerAmount = 500;
 
-  const escrowAccount = anchor.web3.Keypair.generate();
-  const payer = anchor.web3.Keypair.generate();
+  const marketAccount = anchor.web3.Keypair.generate();
   const mintAuthority = anchor.web3.Keypair.generate();
-  const initializerMainAccount = anchor.web3.Keypair.generate();
+  const hostAccount = anchor.web3.Keypair.generate();
   const takerMainAccount = anchor.web3.Keypair.generate();
+  const payer = anchor.web3.Keypair.generate();
+
 
   const opts = "processed";
   const network = "http://127.0.0.1:8899";
@@ -54,160 +53,55 @@ describe('anchor-escrow', () => {
         tx.add(
           SystemProgram.transfer({
             fromPubkey: payer.publicKey,
-            toPubkey: initializerMainAccount.publicKey,
+            toPubkey: hostAccount.publicKey,
             lamports: 1000000000,
           }),
-          SystemProgram.transfer({
-            fromPubkey: payer.publicKey,
-            toPubkey: takerMainAccount.publicKey,
-            lamports: 1000000000,
-          })
         );
         return tx;
       })(),
       [payer]
     );
 
-    mintA = await Token.createMint(
+    mint = await Token.createMint(
       provider.connection,
       payer,
       mintAuthority.publicKey,
       null,
-      0,
+      100,
       TOKEN_PROGRAM_ID
     );
 
-    mintB = await Token.createMint(
-      provider.connection,
-      payer,
-      mintAuthority.publicKey,
-      null,
-      0,
-      TOKEN_PROGRAM_ID
-    );
+    initializerTokenAccount1 = await mint.createAccount(hostAccount.publicKey);
+    initializerTokenAccount2 = await mint.createAccount(hostAccount.publicKey);
 
-    initializerTokenAccountA = await mintA.createAccount(initializerMainAccount.publicKey);
-    takerTokenAccountA = await mintA.createAccount(takerMainAccount.publicKey);
-
-    initializerTokenAccountB = await mintB.createAccount(initializerMainAccount.publicKey);
-    takerTokenAccountB = await mintB.createAccount(takerMainAccount.publicKey);
-
-    await mintA.mintTo(
-      initializerTokenAccountA,
+    await mint.mintTo(
+      initializerTokenAccount1,
       mintAuthority.publicKey,
       [mintAuthority],
       initializerAmount
     );
 
-    await mintB.mintTo(
-      takerTokenAccountB,
+    await mint.mintTo(
+      initializerTokenAccount2,
       mintAuthority.publicKey,
       [mintAuthority],
       takerAmount
     );
 
-    let _initializerTokenAccountA = await mintA.getAccountInfo(initializerTokenAccountA);
-    let _takerTokenAccountB = await mintB.getAccountInfo(takerTokenAccountB);
+    let account1 = await mint.getAccountInfo(initializerTokenAccount1);
+    let account2 = await mint.getAccountInfo(initializerTokenAccount2);
 
-    assert.ok(_initializerTokenAccountA.amount.toNumber() == initializerAmount);
-    assert.ok(_takerTokenAccountB.amount.toNumber() == takerAmount);
+    assert.ok(account1.amount.toNumber() == initializerAmount);
+    assert.ok(account2.amount.toNumber() == takerAmount);
+    
   });
 
-  it("Initialize escrow", async () => {
-    const [_vault_account_pda, _vault_account_bump] = await PublicKey.findProgramAddress(
-      [Buffer.from(anchor.utils.bytes.utf8.encode("token-seed"))],
-      program.programId
-    );
-    vault_account_pda = _vault_account_pda;
-    vault_account_bump = _vault_account_bump;
-
-    const [_vault_authority_pda, _vault_authority_bump] = await PublicKey.findProgramAddress(
-      [Buffer.from(anchor.utils.bytes.utf8.encode("escrow"))],
-      program.programId
-    );
-    vault_authority_pda = _vault_authority_pda;
-
-    await program.rpc.initialize(
-      vault_account_bump,
-      new anchor.BN(initializerAmount),
-      new anchor.BN(takerAmount),
-      {
-        accounts: {
-          initializer: initializerMainAccount.publicKey,
-          vaultAccount: vault_account_pda,
-          mint: mintA.publicKey,
-          initializerDepositTokenAccount: initializerTokenAccountA,
-          initializerReceiveTokenAccount: initializerTokenAccountB,
-          escrowAccount: escrowAccount.publicKey,
-          systemProgram: anchor.web3.SystemProgram.programId,
-          rent: anchor.web3.SYSVAR_RENT_PUBKEY,
-          tokenProgram: TOKEN_PROGRAM_ID,
-        },
-        instructions: [
-          await program.account.escrowAccount.createInstruction(escrowAccount),
-        ],
-        signers: [escrowAccount, initializerMainAccount],
-      }
-    );
-
-    let _vault = await mintA.getAccountInfo(vault_account_pda);
-
-    let _escrowAccount = await program.account.escrowAccount.fetch(
-      escrowAccount.publicKey
-    );
-
-    // Check that the new owner is the PDA.
-    assert.ok(_vault.owner.equals(vault_authority_pda));
-
-    // Check that the values in the escrow account match what we expect.
-    assert.ok(_escrowAccount.initializerKey.equals(initializerMainAccount.publicKey));
-    assert.ok(_escrowAccount.initializerAmount.toNumber() == initializerAmount);
-    assert.ok(_escrowAccount.takerAmount.toNumber() == takerAmount);
-    assert.ok(
-      _escrowAccount.initializerDepositTokenAccount.equals(initializerTokenAccountA)
-    );
-    assert.ok(
-      _escrowAccount.initializerReceiveTokenAccount.equals(initializerTokenAccountB)
-    );
-  });
-
-  it("Exchange escrow state", async () => {
-    await program.rpc.exchange({
-      accounts: {
-        taker: takerMainAccount.publicKey,
-        takerDepositTokenAccount: takerTokenAccountB,
-        takerReceiveTokenAccount: takerTokenAccountA,
-        initializerDepositTokenAccount: initializerTokenAccountA,
-        initializerReceiveTokenAccount: initializerTokenAccountB,
-        initializer: initializerMainAccount.publicKey,
-        escrowAccount: escrowAccount.publicKey,
-        vaultAccount: vault_account_pda,
-        vaultAuthority: vault_authority_pda,
-        tokenProgram: TOKEN_PROGRAM_ID,
-      },
-      signers: [takerMainAccount]
-    });
-
-    let _takerTokenAccountA = await mintA.getAccountInfo(takerTokenAccountA);
-    let _takerTokenAccountB = await mintB.getAccountInfo(takerTokenAccountB);
-    let _initializerTokenAccountA = await mintA.getAccountInfo(initializerTokenAccountA);
-    let _initializerTokenAccountB = await mintB.getAccountInfo(initializerTokenAccountB);
-
-    //console.log(_takerTokenAccountA.owner, _initializerTokenAccountA.owner, _takerTokenAccountB.owner, _initializerTokenAccountB.owner);
-    //console.log(takerTokenAccountA, initializerTokenAccountA, takerTokenAccountB, initializerTokenAccountB);
-    //console.log(_initializerTokenAccountA.address._bn);
+  it("List state", async () => {
     const conn = new web3.Connection(network, opts);
-    //console.log(initializerMainAccount.publicKey);
-    let allMyNFTs = await NFTs.getMintTokensByOwner(conn, initializerMainAccount.publicKey);
-    //console.log('allMyNFTs', allMyNFTs);
+    let allMyNFTs = await NFTs.getMintTokensByOwner(conn, hostAccount.publicKey);
 
     allMyNFTs.forEach(async element => {
       let mintPublicKey = toPublicKey(element);
-      //console.log(mintPublicKey);
-      //let temp = await NFTs.getMintTokenOwner(conn, mintPublicKey);
-      //console.log(temp);
-
-      //let largestAccs = [];
       try {
         let largestAccs = await conn.getTokenLargestAccounts(mintPublicKey, opts);
         console.log(element, largestAccs.value[0].amount);
@@ -217,75 +111,66 @@ describe('anchor-escrow', () => {
         };
       }
     });
-
-    
-
-    assert.ok(_takerTokenAccountA.amount.toNumber() == initializerAmount);
-    assert.ok(_initializerTokenAccountA.amount.toNumber() == 0);
-    assert.ok(_initializerTokenAccountB.amount.toNumber() == takerAmount);
-    assert.ok(_takerTokenAccountB.amount.toNumber() == 0);
   });
 
-  it("Initialize escrow and cancel escrow", async () => {
-    // Put back tokens into initializer token A account.
-    await mintA.mintTo(
-      initializerTokenAccountA,
-      mintAuthority.publicKey,
-      [mintAuthority],
-      initializerAmount
+
+  it("Initialize marketplace", async () => {
+    const [_vault_account_pda, _vault_account_bump] = await PublicKey.findProgramAddress(
+      [Buffer.from(anchor.utils.bytes.utf8.encode("marketplace"))],
+      program.programId
     );
+    vault_account_pda = _vault_account_pda;
+    vault_account_bump = _vault_account_bump;
+
+    const [_vault_authority_pda, _vault_authority_bump] = await PublicKey.findProgramAddress(
+      [Buffer.from(anchor.utils.bytes.utf8.encode("market"))],
+      program.programId
+    );
+    vault_authority_pda = _vault_authority_pda;
 
     await program.rpc.initialize(
       vault_account_bump,
       new anchor.BN(initializerAmount),
-      new anchor.BN(takerAmount),
       {
         accounts: {
-          initializer: initializerMainAccount.publicKey,
+          initializer: hostAccount.publicKey,
           vaultAccount: vault_account_pda,
-          mint: mintA.publicKey,
-          initializerDepositTokenAccount: initializerTokenAccountA,
-          initializerReceiveTokenAccount: initializerTokenAccountB,
-          escrowAccount: escrowAccount.publicKey,
+          mint: mint.publicKey,
+          marketAccount: marketAccount.publicKey,
           systemProgram: anchor.web3.SystemProgram.programId,
           rent: anchor.web3.SYSVAR_RENT_PUBKEY,
           tokenProgram: TOKEN_PROGRAM_ID,
         },
         instructions: [
-          await program.account.escrowAccount.createInstruction(escrowAccount),
+          await program.account.marketAccount.createInstruction(marketAccount),
         ],
-        signers: [escrowAccount, initializerMainAccount],
+        signers: [marketAccount, hostAccount],
       }
     );
 
-    // Cancel the escrow.
-    await program.rpc.cancel({
+    let _marketAccount = await program.account.marketAccount.fetch(
+      marketAccount.publicKey
+    );
+
+    // Check that the values in the escrow account match what we expect.
+    assert.ok(_marketAccount.initializerKey.equals(hostAccount.publicKey));
+    assert.ok(_marketAccount.amount.toNumber() == initializerAmount);
+  });
+
+  it("Buy state", async () => {
+    await program.rpc.buy({
       accounts: {
-        initializer: initializerMainAccount.publicKey,
-        initializerDepositTokenAccount: initializerTokenAccountA,
+        initializer: hostAccount.publicKey,
         vaultAccount: vault_account_pda,
-        vaultAuthority: vault_authority_pda,
-        escrowAccount: escrowAccount.publicKey,
+        mint: mint.publicKey,
+        marketAccount: marketAccount.publicKey,
         tokenProgram: TOKEN_PROGRAM_ID,
       },
-      signers: [initializerMainAccount]
+      signers: [hostAccount]
     });
 
-    // Check the final owner should be the provider public key.
-    const _initializerTokenAccountA = await mintA.getAccountInfo(initializerTokenAccountA);
+    //initializerTokenAccount2 = await mint.createAccount(marketAccount.publicKey);
     
-    assert.ok(_initializerTokenAccountA.owner.equals(initializerMainAccount.publicKey));
-
-    // Check all the funds are still there.
-    assert.ok(_initializerTokenAccountA.amount.toNumber() == initializerAmount);
-
-
-    // Or use this function with pagination and caching
-    /*let page = 1;
-    const perPage = 10;
-    const cacheTtlMins = 1; // Will keep the mint tokens cached for 1 minute
-    console.log(_initializerTokenAccountA.owner);
-    let myNFTsPage1 = await NFTs.getNFTsByOwner(conn, _initializerTokenAccountA.owner, page, perPage, cacheTtlMins);
-    console.log('myNFTsPage1', myNFTsPage1);*/
+    console.log(marketAccount);
   });
 });
